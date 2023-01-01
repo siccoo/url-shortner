e0c83aa1e288a235313856a8fd6617308aacdd16
Michael Chilaka <obichilaka@gmail.com>
2023-01-01 08:08:04 +0100

    updates on inputShortner.jsx, App.js, App.css

--- src/components/inputShortner/InputShortner.jsx
+++ src/components/inputShortner/InputShortner.jsx
@@ -1,9 +1,15 @@
 const InputShortner = () => {
   return (
     <div className="inputContainer">
-<h1>URL <span>Shortner</span></h1>
+      <h1>
+        URL <span>Shortner</span>
+      </h1>
+      <div>
+        <input type="text" placeholder="Enter link to be shortened" />
+        <button>Shorten</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default InputShortner
+export default InputShortner;
